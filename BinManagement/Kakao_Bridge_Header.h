3550dfa8ab258bcb160cd46bcb5c5a6ca7bfef7b
hwiwls <80039139+hwiwls@users.noreply.github.com>
2023-09-25 16:48:16 +0900

    .gitignore 추가

---
+++ BinManagement/Kakao_Bridge_Header.h
@@ -0,0 +1,13 @@
+//
+//  Kakao_Bridge_Header.h
+//  BinManagement
+//
+//  Created by hwijinjeong on 2023/09/23.
+//
+
+#ifndef Kakao_Bridge_Header_h
+#define Kakao_Bridge_Header_h
+
+#import <DaumMap/MTMapView.h> 
+
+#endif /* Kakao_Bridge_Header_h */
--- BinManagement/ViewController.swift
+++ BinManagement/ViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MTMapViewDelegate {
+    
+    var mapView: MTMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView = MTMapView(frame: self.view.frame)
+        mapView.delegate = self
+        mapView.baseMapType = .standard
+        self.view.addSubview(mapView)
     }
 
 
