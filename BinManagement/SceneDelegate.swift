d256986cf9d3046329056f25263bba4b49344256
hwiwls <80039139+hwiwls@users.noreply.github.com>
2023-11-15 22:44:52 +0900

    [HTF-30] [HTF-31] 헤더 만들기, CollectionView 만들기

--- BinManagement/SceneDelegate.swift
+++ BinManagement/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = BinStatusViewController()
+        window?.rootViewController = SearchBuildingViewController()
         window?.makeKeyAndVisible()
     }
 
